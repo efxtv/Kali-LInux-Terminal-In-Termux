73c7f0ed118faaa6356fcb8ad5469ebaf304036e
Tech EFX Tv <59799893+efxtv@users.noreply.github.com>
2022-06-20 16:19:06 +0530

    Create Directory-generate.sh

---
+++ basgrc/Directory-generate.sh
@@ -0,0 +1,7 @@
+#🤐 copy the script to following location👇 #/data/data/com.termux/files/usr/etc/profile.d/
+
+#It will generate directory EFXTv in the home directory automatically
+DIR="/data/data/com.termux/files/home/EFXTv"
+if [ -d "$DIR" ]; then
+mkdir -p /data/data/com.termux/files/home/EFXTV
+fi
